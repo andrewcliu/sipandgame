e1259378700adf79c0db47e42c1c5ef6aaccc0a2
Christian Chastain <chastain_christian@hotmail.com>
2026-03-06 12:24:46 -0800

    Render Google Calendar events on homepage via FullCalendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,33 @@
 require "net/http"
+require "uri"
 require "icalendar"
 
 class EventsController < ApplicationController
-  def feed
-    ics_url = Rails.application.credentials.dig(:google, :ics_url)
+  def index
+  end
 
-    uri = URI(ics_url)
+  def feed
+    ics_url = ENV["GOOGLE_ICS_URL"]
+    return render(json: { error: "Missing GOOGLE_ICS_URL env var" }, status: 500) if ics_url.blank?
+
+    uri = URI.parse(ics_url)
     ics_data = Net::HTTP.get(uri)
 
-    calendars = Icalendar::Calendar.parse(ics_data)
-    cal = calendars.first
+    cal = Icalendar::Calendar.parse(ics_data).first
 
     events = cal.events.map do |e|
       {
+        id: e.uid.to_s,
         title: e.summary.to_s,
-        start: e.dtstart.to_time.iso8601,
-        end:   (e.dtend ? e.dtend.to_time.iso8601 : nil),
+        start: e.dtstart&.to_time&.iso8601,
+        end: (e.dtend ? e.dtend.to_time.iso8601 : nil),
         description: e.description.to_s,
         location: e.location.to_s
       }
     end
 
     render json: events
+  rescue => ex
+    render json: { error: ex.message }, status: 500
   end
 end
-
